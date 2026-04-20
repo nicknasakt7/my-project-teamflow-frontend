62cd0161b2e827b18f48b76428a158120d2c2654
nicknasakt7 <tntkt7@hotmail.com>
2026-04-20 16:30:31 +0700

    feat: Implement cron job to send request every 10 miutes

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
+  console.log('Executed');
   const authHeader = request.headers.get('authorization');
 
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
